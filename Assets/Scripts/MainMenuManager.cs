7b15f1aa1ec9c849964a1eca356166a2ff846237
Mariz-c <stellamaris2004@gmail.com>
2025-11-17 00:26:34 +0800

    trans?

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -114,7 +114,7 @@ public class MainMenuManager : MonoBehaviour
 
         // Load the transition scene (which will apply tank configuration)
         Debug.Log("Starting game with current tank configuration...");
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Transition");
     }
 
     public void QuitGame()
