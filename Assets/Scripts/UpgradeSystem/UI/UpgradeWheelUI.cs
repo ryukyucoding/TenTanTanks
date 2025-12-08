252ad95a0274e31ebd4b14602480e2657bdfee01
Mariz-c <stellamaris2004@gmail.com>
2025-12-08 23:36:17 +0800

    -180f

--- Assets/Scripts/UpgradeSystem/UI/UpgradeWheelUI.cs
+++ Assets/Scripts/UpgradeSystem/UI/UpgradeWheelUI.cs
@@ -721,7 +721,7 @@ public class UpgradeWheelUI : MonoBehaviour
         }
 
         float angleStep = 120f;
-        float startAngle = -150f;
+        float startAngle = -180f;
 
         for (int i = 0; i < options.Count; i++)
         {
